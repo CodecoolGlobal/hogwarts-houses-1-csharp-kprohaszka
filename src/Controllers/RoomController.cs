b1205ded2793b25aa7519d3a3560befee0f6f89d
cc.kprohaszka@gmail.com <cc.kprohaszka@gmail.com>
2021-10-22 13:06:48 +0200

    feat: Create GetAllRooms in Room Controller

---
+++ src/Controllers/RoomController.cs
@@ -0,0 +1,27 @@
+﻿
+using HogwartsHouses.DAL;
+using HogwartsHouses.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace HogwartsHouses.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RoomController : Controller
+    {
+        private readonly IRepository<Room> _repository;
+
+        public RoomController(IRepository<Room> repository)
+        {
+            _repository = repository;
+        }
+
+        [HttpGet]
+        public IEnumerable<Room> GetAllRooms()
+        {
+            var rooms = _repository.GetAll();
+            return rooms;
+        }
+    }
+}
