406e5da61e96ce7c31fab2db93bc539544a8e9db
cc.kprohaszka@gmail.com <cc.kprohaszka@gmail.com>
2021-10-25 11:15:08 +0200

    feat: Create Props and Ctor in RoomSampler

--- src/DAL/RoomSampler.cs
+++ src/DAL/RoomSampler.cs
@@ -1,16 +1,21 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using HogwartsHouses.Models;
+using HogwartsHouses.Models.Types;
 
 namespace HogwartsHouses.DAL
 {
     public class RoomSampler
     {
         public HashSet<Room> Rooms { get; private set; }
+        public List<Student> Students { get; private set; }
+        public object PetTypes { get; private set; }
 
         public RoomSampler()
         {
             Rooms = new HashSet<Room>();
+            Students = new List<Student>();
             Initialize();
         }
 
