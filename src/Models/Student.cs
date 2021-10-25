fd352cfe3ebb0f018623f84fbc1851a2da1e328e
cc.kprohaszka@gmail.com <cc.kprohaszka@gmail.com>
2021-10-25 11:22:35 +0200

    feat: Create Ctor and Props in Student Class

--- src/Models/Student.cs
+++ src/Models/Student.cs
@@ -1,7 +1,19 @@
+using HogwartsHouses.Models.Types;
+
 namespace HogwartsHouses.Models
 {
     [System.Serializable]
+
     public class Student
     {
+        public Student(string name, PetType pet)
+        {
+            Name = name;
+            Pet = pet;
+        }
+
+        public string Name { get; set; }
+        public PetType Pet { get; set; }
+
     }
 }
